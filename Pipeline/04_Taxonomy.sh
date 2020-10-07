50cfea7105d39661f05742c028b4147c934938f0
yyz <42016367+zhouyunyan@users.noreply.github.com>
2020-10-07 16:02:18 +0800

    Update 04_Taxonomy.sh

--- Pipeline/04_Taxonomy.sh
+++ Pipeline/04_Taxonomy.sh
@@ -17,7 +17,7 @@ Taxonomy=./04_Taxonomy
 diamond makedb --in ${Database}/uniprot/uniprot_trembl.fasta.gz -d ${Database}/uniprot/uniprot_trembl
 
 #aligning protein sequence of gene catalog to the Uniprot TrEMBL database
-${diamond} blastp -q ${GeneCatalog}/01_before_cdhit/total.protein.faa.90 -d ${Database}/uniprot/uniprot_trembl.dmnd -t tmp -p 8 -e 1e-5 -k 50 --id 30 --sensitive -o ${Taxonomy}/total.protein.faa.90.diamond2uniprot_trembl    
+${diamond} blastp -q ${GeneCatalog}/02_cdhit_cluster/total.protein.faa.90 -d ${Database}/uniprot/uniprot_trembl.dmnd -t tmp -p 8 -e 1e-5 -k 50 --id 30 --sensitive -o ${Taxonomy}/total.protein.faa.90.diamond2uniprot_trembl    
 
 #taxonomic classification based on the LCA algorithms
 ${basta} sequence -l 25 -i 80 -e 0.00001 -m 3 -b 1 -p 60 ${Taxonomy}/total.protein.faa.90.diamond2uniprot_trembl ${Taxonomy}/PIGC90.diamond2uniprot_trembl.dmnd.lca.out ${Database}/basta/uniprot
